cab4634d511bd9964577ca37fc9f64b79c1a49de
Javier Jeanpool Borja Samaniego <javierborjasamaniego@gmail.com>
2026-02-02 20:20:19 -0500

    feat: estructura inicial backend con Spring Boot y dependencias base

---
+++ backend/src/main/java/com/cibertec/portal_academico/PortalAcademicoApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec.portal_academico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalAcademicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalAcademicoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cibertec/portal_academico/PortalAcademicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec.portal_academico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalAcademicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
